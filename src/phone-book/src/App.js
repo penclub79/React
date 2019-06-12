e61a520ef92e594106ee84dfe1e0447d63a3b560
hocheol <penclub30@gmail.com>
2019-06-12 10:50:35 +0900

    project

---
+++ src/phone-book/src/App.js
@@ -0,0 +1,38 @@
+import React, {Component} from 'react';
+import PhoneForm from './PhoneForm';
+import PhoneInfoList from "./PhoneInfoList";
+
+class App extends Component {
+    id =2
+    state = {
+        information:[]
+    }
+    handleCreate = (data) => {
+        const { information } = this.state;
+        this.setState({
+            information: information.concat({id: this.id++, ...data})
+        })
+    }
+    handleRemove = (id) => {
+        const {information } = this.state;
+        this.setState({
+            information:information.filter(info => info.id !== id)
+        })
+    }
+    render() {
+        const{ information } = this.state;
+        return (
+            <div>
+                <PhoneForm
+                    onCreate={this.handleCreate}
+                />
+                <PhoneInfoList
+                    data={information}
+                    onRemove={this.handleRemove}
+                />
+            </div>
+        );
+    }
+}
+
+export default App;
